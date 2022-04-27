b5286852cf656056c58dfc8672aaae4f1b3be0a2
Kary Bautista <64227007+Kary3008@users.noreply.github.com>
2022-04-27 12:08:41 -0500

    Programa MD, no concluido.

--- ProgramaT.c
+++ ProgramaT.c
@@ -12,19 +12,46 @@ void main(void)
 	
 	for(i=1;i<=num;i++)
 	{
-		if(num%i==0) /*se divide entre todos los numeros, 
+		if(num % i == 0){ /*se divide entre todos los numeros, 
 			si es igual a 0, hay más números divisibles entre ellos*/
-			cont++;
+			//cont++;
+			if(cont>2)
+			{
+				for(i=1;i<=num/2;i++)
+				{
+					if(num % i ==0) //num modulo es igual a 0
+					{
+						printf("%i, ", i); //el for se detiene hasta no tener divisores, e imprime los que son divisores.
+					}	
+				}
+			}
+			else
+			{
+				printf("%i, ", i);
+			}
+		}
+		//	printf("%i, ", i); //el for se detiene hasta no tener divisores, e imprime los que son divisores.
 	}
+	//printf("%i", num);//se imprime el número porque es divisor entre sí mismo.
 	
-	if(cont>2)//si el conteo es mayor a 2, no es primo
+/*	if(cont>2)//si el conteo es mayor a 2, no es primo
 	//porque quiere decir que hay más números divisibles entre ese número
-		printf("\nEl numero no es primo.");
-	else
-		printf("\nEl numero es primo.");
+		{
+		//printf("\nEl numero no es primo.");
+		for(i=1;i<=num/2;i++)//hasta el num divido entre 2, comprobar de uno en uno si el valor es divisible.
+		{
+			if(num % i ==0) //num modulo es igual a 0
+			{
+				printf("%i, ", i); //el for se detiene hasta no tener divisores, e imprime los que son divisores.
+			}
+		}
+		printf("%i", num);//se imprime el número porque es divisor entre sí mismo.
+	}
+	//else
+		//printf("\nEl numero es primo.");*/
 }
 //Determinar los divisores de un numero.
-
+/*
 void main(void)
 {
 	int i, num;
@@ -35,12 +62,5 @@ void main(void)
 	scanf("%i", &num);
 	}while(num<=0);
 	
-	for(i=1;i<=num/2;i++)//hasta el num divido entre 2, comprobar de uno en uno si el valor es divisible.
-	{
-		if(num % i ==0) //num modulo es igual a 0
-		{
-			printf("%i, ", i); //el for se detiene hasta no tener divisores, e imprime los que son divisores.
-		}
-	}
-	printf("%i", num);//se imprime el número porque es divisor entre sí mismo.
-}
+	
+}*/
